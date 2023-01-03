39d68e8a527f81a63850d77e903a1f94a89d3db6
Kevin Kinner <kevin-kinner@web.de>
2023-01-03 17:36:11 +0100

    Done

--- Python/lengthMetricToImperial/metricToImperial.py
+++ Python/lengthMetricToImperial/metricToImperial.py
@@ -1,35 +1,59 @@
-from distutils.command.build import build
-from logging import root
-from msilib.schema import ComboBox
-from optparse import Values
-from pydoc import text
 from tkinter import *
 from tkinter import ttk
-from turtle import title
-from decimal import Decimal
-import keyboard
-
-metValue = 0
-impValue = 0
-metUnit = ""
-impUnit = ""
-
+from tkinter import messagebox
 
 class Calculation:
-    def cmToInch():
-        result = metValue / impValue
-        return result
+    def metricToImp(metValue):
+        result = round(metValue / 2.54, 2)
+        return str(result)
 
-    def unitChooser():
-        result = 0
-        if metUnit == "cm" and impUnit == "inch":
-            result = Calculation.cmToInch()
-        
-        return result
+    def impToMetric(impValue):
+        result = round(impValue * 2.54, 2)
+        return str(result)
 
+    #checks if the input numbers are floatingpoint
+    def inputValidation(met, imp):
+        try:
+            float(met.get())
+        except ValueError:
+            messagebox.showerror("Not a Number", "The entered value of" + met.get() + "is not a Floatingpoint number!")
+            return False
+        try:
+            float(imp.get())
+        except ValueError:
+            messagebox.showerror("Not a Number", "The entered value of" + imp.get() + "is not a Floatingpoint number!")
+            return False
 
-class Aplication:
+    #declares calulation direction
+    def chooser(direction, met, imp, metChooser):
+        if (Calculation.inputValidation(met, imp) == False):
+            return
+
+        if (direction == "metric -> imperial"):
+            if (metChooser == "m"):
+                imp.delete(0, "end");
+                imp.insert(0 ,Calculation.metricToImp(float(met.get())*100)) 
+            elif (metChooser == "km"):
+                imp.delete(0, "end")
+                imp.insert(0, Calculation.metricToImp(float(met.get())*100000))
+            else:
+                imp.delete(0, "end")
+                imp.insert(0, Calculation.metricToImp(float(met.get())))
+
+        elif (direction == "imperial -> metric"):
+            if (metChooser == "m"):
+                met.delete(0, "end");
+                met.insert(0 ,Calculation.metricToImp(float(imp.get())/100)) 
+            elif (metChooser == "km"):
+                met.delete(0, "end")
+                met.insert(0, Calculation.metricToImp(float(imp.get())/100000))
+            else:
+                met.delete(0, "end")
+                met.insert(0, Calculation.metricToImp(float(imp.get())))
+
+class Aplication():
     def window():
+
         #Window constraints
         root = Tk()
         root.title("Metric to Imperial")
@@ -40,37 +64,33 @@ class Aplication:
         metChooser = ttk.Combobox(frm,values=("cm", "m", "km"),justify="center",state="readonly");
         metChooser.grid(column=0, row=0)
         metChooser.current(0)
+        
 
         #Metric user input / program output 
         metText = ttk.Entry(frm,justify="center")
         metText.grid(column=1, row=0)
         metText.insert(0,1)
+        
+        
         #Imperial user input/program output
         impText = ttk.Entry(frm,justify="center")
         impText.grid(column=2, row=0)
-        impText.insert(0,2.54)
+        impText.insert(0,0.39)
+        
 
         #Imperial unit chooser
-        impChooser = ttk.Combobox(frm, values=("inch", "feet + inch"),justify="center",state="readonly")
+        impChooser = ttk.Combobox(frm, values=("inch"),justify="center",state="readonly")
         impChooser.grid(column=3, row=0)
         impChooser.current(0)
 
-        #set global variables to user input on enter
-        
-        def keyPress():
-            metValue = Decimal(metText.get())
-            impValue = Decimal(impText.get())
-            metUnit = metChooser.get()
-            impUnit = impChooser.get()
-            print(Calculation.unitChooser())
+        #Choose in wich direction you want to convert
+        directionChooser = ttk.Combobox(frm, values=("metric -> imperial","imperial -> metric"), justify="center", state="readonly")
+        directionChooser.grid(column=1, row=1)
+        directionChooser.current(0)
+
+        #Button to run calculation and give parameters to Calc class
+        calcButton = ttk.Button(text="calculate", command= lambda:Calculation.chooser(directionChooser.get(), metText, impText, metChooser.get()))
+        calcButton.grid(column=1, row=2)
+
         root.mainloop()
-
     window()
-
-while True:  # making a loop
-    try:  # used try so that if user pressed other than the given key error will not be shown
-        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
-            print('You Pressed A Key!')
-            break  # finishing the loop
-    except:
-        break  # if user pressed a key other than the given key the loop will break
